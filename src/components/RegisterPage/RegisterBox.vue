9739ab3d356fa29cde31101ef2c8bd886c8d24c5
fadhilzamanNow <fadhilisfadhillah@gmail.com>
2025-04-06 00:37:58 +0700

    refactor : done login and register responsive

--- src/components/RegisterPage/RegisterBox.vue
+++ src/components/RegisterPage/RegisterBox.vue
@@ -1,48 +1,77 @@
 <script setup lang="ts">
-import { Flex } from 'ant-design-vue';
-import { Input } from 'ant-design-vue';
+import { EyeInvisibleOutlined, EyeOutlined } from '@ant-design/icons-vue';
+import { Button, Input } from 'ant-design-vue';
 import { ref } from 'vue';
 import { RouterLink } from 'vue-router';
 const userVal = ref<string>("");
 const emailVal = ref<string>("");
 const passVal = ref<string>("");
+const passShow = ref<boolean>(false);
+const cPassVal = ref<string>("");
+const cPassShow = ref<boolean>(false);
 
 
 </script>
 
 <template>
-     <Flex class="w-[883px] h-[653px] shadow-2xl rounded-lg" gap="24">
-            <div class="w-[392px] h-[653px] bg-[#CFC922] rounded-lg">
+    <div class="flex w-full items-center justify-center  ">
+    <div class="flex flex-col h-full gap-4 w-full lg:rounded-md lg:border lg:max-w-[640px] lg:border-gray-200 lg:mx-20 lg:py-20 lg:shadow-md">
+      <div class="text-center text-[#1890FF] font-semibold text-xl">Daftar</div>
+      <div class="flex flex-col gap-2 items-center justify-center" >
+          <label for="email" class="text-xs w-[90%] ">Username</label>
+            <div class="w-[90%] " >
+              <Input v-model:value="userVal" placeholder="Username" id="username"  />
             </div>
-            <Flex vertical class="pt-20  flex-1" gap="16"  >
-                <h1 class="font-bold text-2xl">Daftar</h1>
-                <Flex vertical gap="8">
-                    <label for="username" class="text-xs">Nama Pengguna</label>
-                    <div class="w-[80%] ">
-                        <Input v-model:value="userVal" placeholder="Username" id="username" />
-                    </div>
-                </Flex>
-                <Flex vertical gap="8">
-                    <label for="email" class="text-xs">Alamat Email</label>
-                    <div class="w-[80%] ">
-                        <Input v-model:value="emailVal" placeholder="Email" id="email" />
-                    </div>
-                </Flex>
-                <Flex vertical gap="8">
-                    <label for="password" class="text-xs">Password</label>
-                    <div class="w-[80%] ">
-                        <Input v-model:value="passVal" placeholder="Password" id="password" />
-                    </div>
-                </Flex>
-                <Flex class="rounded-md bg-[#CFC922] text-white w-[58px] px-5 py-1.5 hover:bg-[#9e9c55] transition duration-200" justify="center" align="center">
-                    Daftar
-                </Flex>
-                <Flex gap="4">
-                    <span>Sudah memiliki akun?</span>
-                    <RouterLink to="/login">
-                        <span class="text-[#CFC922]">Login</span>
-                    </RouterLink>
-                </Flex>
-            </Flex>
-        </Flex>
+      </div>
+      <div class="flex flex-col gap-2 items-center justify-center" >
+          <label for="email" class="text-xs w-[90%] ">Alamat Email</label>
+            <div class="w-[90%] " >
+              <Input v-model:value="emailVal" placeholder="Email" id="email"  />
+            </div>
+      </div>
+      <div class="flex flex-col gap-2 items-center justify-center">
+          <label for="password" class="text-xs w-[90%] ">Password</label>
+            <div class="w-[90%] " >
+              <Input v-model:value="passVal" placeholder="******" id="password" :type="passShow ? 'text' : 'password'">
+                <template #suffix>
+                  <div v-on:click="() => passShow = !passShow">
+                    <EyeInvisibleOutlined v-if="passShow"  />
+                    <EyeOutlined v-if="!passShow"  />
+                  </div>
+                </template>
+              </Input>
+            </div>
+      </div>
+      <div class="flex flex-col gap-2 items-center justify-center">
+          <label for="password" class="text-xs w-[90%] ">Konfirmasi Password</label>
+            <div class="w-[90%] " >
+              <Input v-model:value="cPassVal" placeholder="******" id="password" :type="cPassShow ? 'text' : 'password'">
+                <template #suffix>
+                  <div v-on:click="() => cPassShow = !cPassShow">
+                    <EyeInvisibleOutlined v-if="cPassShow"  />
+                    <EyeOutlined v-if="!cPassShow"  />
+                  </div>
+                </template>
+              </Input>
+            </div>
+      </div>
+      <div class="flex gap-2 items-center justify-center ">
+        <div class="w-[90%]">
+        <RouterLink to="/login">
+            <Button type="primary" class="!bg-black hover:!bg-slate-700">
+              <span>Daftar</span>
+            </Button>
+          </RouterLink>
+        </div>
+      </div>
+      <div class="flex gap-2 items-center justify-center">
+        <div class="w-[90%] flex gap-x-1">
+          <span>Belum memiliki akun?</span>
+          <RouterLink to="/login">
+              <span class="text-[#1890FF]">Login</span>
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
 import { Flex } from 'ant-design-vue';
 import RegisterBox from '../components/RegisterPage/RegisterBox.vue';
+import FooterWave from '../components/Footer/FooterWave.vue';
+import Navbar from '../components/Navbar/Navbar.vue';
+import { motion } from 'motion-v';
 
 </script>
 
 <template>
-    <Flex justify="center" align="center" class="h-[100vh]">
-       <RegisterBox />
-    </Flex>
+    <motion.div
+        :initial="{x : 200, opacity : 0}"
+        :animate="{x: 0, opacity : 1}"
+        :exit="{x : -200, opacity : 0}"
+        :transition="{type : 'spring', stiffness : 260, damping : 20}"
+    >
+        <Flex vertical gap="2" class="h-[100vh] ">
+            <Navbar />
+            <div class="h-full flex pt-10">
+                <RegisterBox class="h-max" />
+            </div>
+            <div>
+                <FooterWave />
+            </div>
+        </Flex>
+    </motion.div>
 </template>
