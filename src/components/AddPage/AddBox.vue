56a92def456736e7b17ea8c26a91554dde068158
fadhilzamanNow <fadhilisfadhillah@gmail.com>
2025-04-08 12:26:36 +0700

    refactor : done addpage responsive

--- src/components/AddPage/AddBox.vue
+++ src/components/AddPage/AddBox.vue
@@ -1,25 +1,21 @@
 <script setup lang="ts">
-import { InboxOutlined, LoadingOutlined } from '@ant-design/icons-vue';
+import { HomeOutlined, InboxOutlined, LoadingOutlined } from '@ant-design/icons-vue';
 import { Flex, Steps } from 'ant-design-vue';
 import { h } from 'vue';
 import { Input, Select, Textarea, DatePicker, UploadDragger, Button } from 'ant-design-vue';
 
-const description = 'This is a description.';
 
 const myItems = [
       {
         title: 'Finished',
-        description,
       },
       {
         title: 'In Progress',
-        description,
         icon : h(LoadingOutlined)
       },
       {
         title: 'Waiting',
-        description,
-      },
+       },
     ]
 
 const option = ["Handphone","Laptop","Dompet","Lain Lain"].map((d) => {
@@ -27,31 +23,35 @@ const option = ["Handphone","Laptop","Dompet","Lain Lain"].map((d) => {
         value : d
     }
 })
+
+
 </script>
 
 <template>
-    <Flex class="p-10.5" vertical justify="center" align="center" gap="40">
-        <div class="w-[800px] text-[8px]" >
-            <Steps :current="2" :items="myItems"  />
-        </div>
-        <Flex vertical gap="16">
-            <Flex vertical class="w-[400px]" gap="8">
+    <div class="flex p-1 flex-col items-center justify-center  gap-6  w-full " >
+        <div class="flex items-center gap-2 text-gray-400 self-start">
+            <HomeOutlined class="text-lg" />
+            <span class="text-sm">Home</span>
+         </div>
+        <div  class="flex flex-col gap-4 sm:border sm:max-w-[640px] sm:border-gray-200 w-full justify-center  rounded-md p-4">
+           <!--  <Steps :current="0" :items="myItems.reverse()" size="small"  label-placement="vertical" direction="horizontal" type="navigation" progress-dot /> -->
+            <Flex vertical class="w-full " gap="8">
                 <label for="item">Nama Barang</label>
                 <Input placeholder="Nama Barang" />
             </Flex>
-            <Flex vertical class="w-[400px]" gap="8">
+            <Flex vertical class="w-full " gap="8">
                 <label for="item">Kategori Barang</label>
                 <Select placeholder="Pilih Kategori Barang" :options="option" />
             </Flex>
-            <Flex vertical class="w-[400px]" gap="8">
+            <Flex vertical class="w-full " gap="8">
                 <label for="item">Deskripsi Barang</label>
                 <Textarea placeholder="Deskripsi Barang"  />
             </Flex>
-            <Flex vertical class="w-[400px]" gap="8">
+            <Flex vertical class="w-full " gap="8">
                 <label for="item">Tanggal Kehilangan</label>
                 <DatePicker format="YYYY-MM-DD" placement="bottomRight" />
             </Flex>
-            <Flex vertical class="w-[400px]" gap="8">
+            <Flex vertical class="w-full" gap="8">
                 <label for="item">Foto Barang</label>
                 <UploadDragger>
                     <InboxOutlined />
@@ -63,7 +63,6 @@ const option = ["Handphone","Laptop","Dompet","Lain Lain"].map((d) => {
                     <Button type="primary">Submit</Button>
                 </div>
             </Flex>
-        </Flex>
-
-    </Flex>
+        </div>
+    </div>
 </template>
--- src/pages/AddPage.vue
+++ src/pages/AddPage.vue
@@ -4,6 +4,10 @@ import Sidebar from "../components/Sidebar/Sidebar.vue";
 import Navbar from "../components/Navbar/Navbar.vue";
 import { onMounted, onUnmounted } from "vue";
 import AddBox from "../components/AddPage/AddBox.vue";
+import { useSidebarStore } from "../stores/sidebarInfo";
+import FooterWave from "../components/Footer/FooterWave.vue";
+
+const sidebar = useSidebarStore()
 
 onMounted(() => {
     console.log('Enter Add Page');
@@ -15,11 +19,20 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <Flex>
-    <Sidebar active="Add" />
-    <Flex flex="1" vertical>
+  <div class="flex relative h-screen">
+    <div class="h-full">
+      <Sidebar active="Home" :class="[sidebar.isExpand ? 'absolute z-10 ' : '']"  />
+    </div>
+    <div class="h-full flex-1 flex flex-col">
       <Navbar />
-      <AddBox />
-    </Flex>
-  </Flex>
+      <div class="flex flex-col h-full gap-6 p-4">
+        <div class="flex-1">
+          <AddBox />
+        </div>
+    </div>
+    <div class="w-full">
+            <FooterWave />
+        </div>
+      </div>
+  </div>
 </template>
