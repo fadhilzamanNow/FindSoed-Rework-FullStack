5aef4f6cbe6e01b4f4def7b0c305752c118bdc79
fadhilzamanNow <fadhilisfadhillah@gmail.com>
2025-06-02 18:51:00 +0700

    feat : add search in list

--- src/components/ListPage/HeaderListPage.vue
+++ src/components/ListPage/HeaderListPage.vue
@@ -4,31 +4,100 @@ import {
   MinusOutlined,
   SearchOutlined,
 } from "@ant-design/icons-vue";
-import { Flex, Input } from "ant-design-vue";
-import { ref } from "vue";
+import { Flex, Input, InputProps, InputSearch } from "ant-design-vue";
+import { computed, ref, watchEffect } from "vue";
 import { useCardStore } from "../../stores/cardInfo";
 import BreadCrumbComp from "../BreadCrumb/BreadCrumbComp.vue";
 import { storeToRefs } from "pinia";
+import { findPost } from "../../api/Post/Post";
+import { usePostStore } from "../../stores/postStore";
+import lodash from "lodash"
 
-const {searchItem} = storeToRefs(useCardStore())
 
 
-const showSearchFilter = ref<boolean>(false);
 const card = useCardStore();
+const search = ref("");
+const post = usePostStore()
+
+
+const debounceSearch = lodash.debounce( async (searchItem : string) => {
+  try{
+    /* const response = await findPost(search);
+    if(response){
+      post.setPost(response.data)
+    } */
+   search.value = searchItem
+   const response = await findPost(search.value);
+   if(response){
+    post.setPost(response.data)
+   }
+  }catch(e){
+
+  }
+},1000)
+
+
+const handleSearchItem = async () => {
+  try{
+    const response = await findPost(search.value)
+    if(response){
+      post.setPost(response.data)
+    }
+  }catch(e){
+
+  }
+}
+
+const inputSearch = computed<InputProps>(() => ({
+  placeholder : "Cari Barang",
+  onInput : (e) => debounceSearch((e.target as HTMLInputElement).value),
+  value : search.value,
+}))
+
+watchEffect(() => {
+  console.log("value : ", search.value)
+})
+
+
+watchEffect(() => {
+  lodash.debounce(async () => {
+    try{
+    const response = await findPost(search.value)
+    if(response){
+      post.setPost(response.data)
+    }
+  }catch(e){
+  }
+  }, 1000)
+})
+
+
+
 </script>
 
 <template>
   <div class="flex flex-col gap-4 relative">
     <div class="flex justify-between">
       <BreadCrumbComp title="" />
-      <div
-        class="max-w-max bg-blue-600 rounded-md flex justify-center items-center text-white p-1.5 px-5 hover:bg-[#188fffad] transition-all duration-500 "
+     <!--  <div
+        class="overflow-x-hidden w-2 hover:w-32 bg-blue-600 rounded-md flex justify-center items-center text-white py-1 px-5 hover:bg-[#188fffad] transition-all duration-500 "
         @click="() => (showSearchFilter = !showSearchFilter)"
       >
+        <div class=" hover:hidden">
+          asdadasasdaadasdad
+        </div>
         <SearchOutlined class="text-white" />
-      </div>
+      </div> -->
+      <div class="w-1/2 lg:w-3/8 ">
+        <Input v-bind="inputSearch" class="h-full relative z-2">
+          <div class="absolute top-1/2 right-2 -translate-y-1/2 text-gray-400 z-[99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999]">
+            <SearchOutlined  />
+          </div>
+        </Input>
+        
+      </div>  
     </div>
-    <div
+    <!-- <div
       class="flex flex-col shadow-md p-2 rounded-md gap-y-2 absolute w-full z-99999999999 bg-white border border-gray-200"
       :class="[showSearchFilter ? 'opacity-100 visible' : ' opacity-0 invisible ','overflow-hidden transition-all duration-300']"
     >
@@ -48,7 +117,7 @@ const card = useCardStore();
           <FilterFilled />
         </Flex>
       </div>
-    </div>
+    </div> -->
     <span class="text-sm text-gray-500"
       >Telah ditemukan {{ card.itemList.length }} barang
     </span>
