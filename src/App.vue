1cee075a3ec683abbbc86831c951d38b06f49878
fadhilzamanNow <fadhilisfadhillah@gmail.com>
2025-06-04 04:14:33 +0700

    feat : add new pages, and components migrate from vue ssr

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,70 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { onMounted, watchEffect } from 'vue';
-import { RouterView,RouterLink } from 'vue-router';
-import { useViewStore } from './stores/viewStore';
-import { useAuthStore } from './stores/authStore';
-import { findUserInfo } from './api/Auth/Auth';
-import { storeToRefs } from 'pinia';
-import { useHead } from '@unhead/vue'
+import { onMounted, watchEffect } from "vue";
+import { RouterView } from "vue-router";
+import { useViewStore } from "./stores/viewStore";
+import { useAuthStore } from "./stores/authStore";
+import { findUserInfo } from "./api/Auth/Auth";
+import { storeToRefs } from "pinia";
+import { useHead } from "@unhead/vue";
 
 useHead({
-  title: 'Findsoed Rework'
-})
+  title: "Findsoed Rework",
+});
 
 const auth = useAuthStore();
-const {authToken} = storeToRefs(auth)
+const { authToken } = storeToRefs(auth);
 
 onMounted(() => {
-
   const appElement = document.getElementById("root");
-  if(appElement){
-    console.log("isi appElement : ", appElement)
+  if (appElement) {
+    console.log("isi appElement : ", appElement);
     appElement.classList.remove("app-loading");
-    appElement.classList.add("app-loaded")
-/*     appElement.classList.add("app-loaded");
- */  }
+    appElement.classList.add("app-loaded");
+    /*     appElement.classList.add("app-loaded");
+     */
+  }
 
   const view = useViewStore();
 
   view.changeView({
-    height : window.innerHeight,
-    width : window.innerWidth
-  })
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
 
-  window.addEventListener('resize',() => {
+  window.addEventListener("resize", () => {
     view.changeView({
-      height : window.innerHeight,
-      width : window.innerWidth
-    })
-  })
+      height: window.innerHeight,
+      width: window.innerWidth,
+    });
+  });
 
   watchEffect(() => {
-  if(authToken){
-    auth.setAuthToken(localStorage.getItem("authToken"))
-    findInfo()
-  }
-})
-})
+    if (authToken) {
+      auth.setAuthToken(localStorage.getItem("authToken"));
+      findInfo();
+    }
+  });
+});
 
 const findInfo = async () => {
-  try{
+  try {
     const response = await findUserInfo();
-    if(response){
+    if (response) {
       auth.setUserInfo({
-        username : response.data.username,
-          userId : response.data.userId,
-          email : response.data.email,
-          imageUrl : response.data.imageUrl,
-          phoneNumber : response.data.phoneNumber
-      })
+        username: response.data.username,
+        userId: response.data.userId,
+        email: response.data.email,
+        imageUrl: response.data.imageUrl,
+        phoneNumber: response.data.phoneNumber,
+      });
     }
-  }catch(e){
-    auth.setAuthToken(null)
-    auth.setUserInfo(null)
+  } catch (e) {
+    auth.setAuthToken(null);
+    auth.setUserInfo(null);
   }
-}
-
-
+};
 </script>
 
-<template>  
-
+<template>
   <RouterView />
 </template>
-
-<style scoped>
-
-</style>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
-import { Flex } from 'ant-design-vue';
-import LoginBox from '../components/LoginPage/LoginBox.vue';
+import { Flex } from "ant-design-vue";
+import LoginBox from "../components/LoginPage/LoginBox.vue";
 /* import Navbar from '../components/Navbar/Navbar.vue'; */
-import { motion } from 'motion-v';
-import FooterWave from '../components/Footer/FooterWave.vue';
-import { ref, watch, watchEffect } from 'vue';
-import { useAuthStore } from '../stores/authStore';
-import { useRouter } from 'vue-router';
-import { storeToRefs } from 'pinia';
-import { useSeoMeta } from '@unhead/vue';
-import Navbar from '../components/LandingPage/Navbar.vue';
-import Footer from '../components/LandingPage/Footer.vue';
+import { ref, watchEffect } from "vue";
+import { useAuthStore } from "../stores/authStore";
+import { useRouter } from "vue-router";
+import { storeToRefs } from "pinia";
+import { useSeoMeta } from "@unhead/vue";
+import Navbar from "../components/LandingPage/Navbar.vue";
+import Footer from "../components/LandingPage/Footer.vue";
 
 useSeoMeta({
-    title : "SSR RSbuild Login Page - Findsoed Rework",
-    description : "SSR Rsbuild Halaman Login Page untuk Findsoed Rework dapat digunakan untuk autentikasi pengguna",
-    ogTitle : "SSR Rsbuild Login Page - Findsoed Rework",
-    ogDescription : "SSR Rsbuild Halaman Login Page untuk Findsoed Rework dapat digunakan untuk autentikasi pengguna",
-    ogUrl : "http://localhost:3500/login",
-    ogSiteName : "Findsoed Rework",
-    ogType : "website",
-    author : "Muhammad Ilham Isfadhillah",
-    twitterTitle : "SSR Rsbuild Login Page - Findsoed Rework",
-    robots : "index, follow"
-})
+  title: "SSR RSbuild Login Page - Findsoed Rework",
+  description:
+    "SSR Rsbuild Halaman Login Page untuk Findsoed Rework dapat digunakan untuk autentikasi pengguna",
+  ogTitle: "SSR Rsbuild Login Page - Findsoed Rework",
+  ogDescription:
+    "SSR Rsbuild Halaman Login Page untuk Findsoed Rework dapat digunakan untuk autentikasi pengguna",
+  ogUrl: "http://localhost:3500/login",
+  ogSiteName: "Findsoed Rework",
+  ogType: "website",
+  author: "Muhammad Ilham Isfadhillah",
+  twitterTitle: "SSR Rsbuild Login Page - Findsoed Rework",
+  robots: "index, follow",
+});
 
 const auth = useAuthStore();
-const {authToken} = storeToRefs(auth);
+const { authToken } = storeToRefs(auth);
 const loginLoading = ref(false);
 
 const navigate = useRouter();
@@ -38,25 +38,22 @@ const navigate = useRouter();
 },{immediate : true}) */
 
 const toggleLoading = () => {
-    loginLoading.value = !loginLoading.value
-}
+  loginLoading.value = !loginLoading.value;
+};
 
 watchEffect(() => {
-    if(authToken.value){
-        navigate.push("/home")
-    }
-})
-
-
+  if (authToken.value) {
+    navigate.push("/home");
+  }
+});
 </script>
 
-
 <template>
-        <Flex vertical gap="2" class="h-[100vh] ">
-            <Navbar />
-            <div class="flex flex-col justify-between h-full ">
-                <LoginBox  @toggle-loading="toggleLoading" />
-                <Footer />
-            </div>
-        </Flex>
+  <Flex vertical gap="2" class="h-[100vh]">
+    <Navbar />
+    <div class="flex flex-col justify-between h-full">
+      <LoginBox @toggle-loading="toggleLoading" />
+      <Footer />
+    </div>
+  </Flex>
 </template>
